6d00deb091776b6c05562d4bbd2fe1df0209f3b1
Felix Habib <fhabib@seek.com.au>
2024-03-25 16:39:16 +1100

    Exercise 4A: Grid layout with Flexbox

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -26,7 +26,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           </Select>
         </Header>
         <Spacer size={34} />
-        {/* <ShoeGrid /> */}
+        <ShoeGrid />
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
